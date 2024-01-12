46e6a773ab893fbd403227c1074017d4920d4ecf
mattheo <mattheotermine104@gmail.com>
2024-01-12 14:04:34 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -10,6 +10,10 @@ $context = stream_context_create($opts);
 // Obtenir l'adresse IP du client
 $clientIP = $_SERVER['REMOTE_ADDR'];
 
+if ($clientIP == '127.0.0.1') {
+    $clientIP = '193.50.135.206';
+}
+
 // Partie 1: Géolocalisation
 // Initialiser cURL pour la première API de géolocalisation
 $chGeo = curl_init();
@@ -30,11 +34,16 @@ $timezone = $geoXmlObj->timezone;
 // Partie 2: Prévisions Météo
 // Initialiser cURL pour la deuxième API de prévisions météo
 $chMeteo = curl_init();
-$meteoApiUrl = "http://www.infoclimat.fr/public-api/gfs/xml?_ll={$latitude},{$longitude}";
+$meteoApiUrl = "http://www.infoclimat.fr/public-api/gfs/xml?_ll={$latitude},{$longitude}&_auth=UUtQR1IsXH5SfwYxBnACK1M7UmdeKAgvC3cAY1o%2FVSgGbVU0UTEHYVU7VyoOIVFnByoFZg41BDRXPFYuCXtTMlE7UDxSOVw7Uj0GYwYpAilTfVIzXn4ILwtpAG5aNFUoBmBVMVEsB2RVOVcwDiBRZwc0BWAOLgQjVzVWNglkUzRROlA9UjBcP1I5Bm0GKQIpU2ZSN15oCGULYQBkWjdVYgZsVTJRNgdkVTtXNA4gUW0HNgVkDjkEPFc8VjUJYlMvUS1QTVJCXCNSfQYmBmMCcFN9UmdePwhk&_c=50405dba53ebc6195952cd0b4a4eba28";
 curl_setopt($chMeteo, CURLOPT_URL, $meteoApiUrl);
 curl_setopt($chMeteo, CURLOPT_RETURNTRANSFER, 1);
 $meteoXML = curl_exec($chMeteo);
 curl_close($chMeteo);
+
+// Vérifiez si la réponse est un XML bien formé
+if (simplexml_load_string($meteoXML) === false) {
+    die("Erreur : la réponse de l'API météo n'est pas un XML bien formé.");
+}
 
 // Charger la feuille de style XSL pour les prévisions météo
 $xslMeteoDoc = new DOMDocument;
